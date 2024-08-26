35ffa1f99c6e0c01cf95d1d5be96c0a0db553f15
petercodess <thecouchpeter@gmail.com>
2024-08-26 21:30:18 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,7 @@ import Intro from './Intro';
 function App() {
   return (
     <>
-      <Nav />
-      <div className='bd h-screen flex justify-center items-center flex-col'>
-        <img src={icon} className='rounded-full' />
-        <h1 className='text-4xl font-bold text-center mt-8 text-slate-950 sam relative'>Danielle's Massage Envy</h1>
-      </div>
-      <Intro />
-      <footer className='bg-slate-900 text-red-600 text-center p-4 flex justify-around md:flex-row gap-3 flex-col' id='bk'>
-        <a href=''>BOOK ME!</a>
-        <p>Cashapp: $JeraimeLowery</p>
-        <p>Paypal: Pevalyn8@gmail.com</p>
-        <p>Venmo: @Rashon29</p>
-        <p>Once you’ve confirmed your payment, send a screenshot to your masseuse to book a session.</p>
-      </footer>
+    <h1 className='text-4xl font-bold text-center mt-8 text-slate-950 sam relative'>werey. </h1>
     </>
   );
 }
